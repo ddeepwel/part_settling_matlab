10fd716f1bd2d72f4d3ec086af1e6145e98d7b3e
David Deepwell <ddeepwel@uwaterloo.ca>
2020-01-15 12:18:26 -0500

    Add colouring for time of particle position

--- plot_particle_positions.m
+++ plot_particle_positions.m
@@ -5,24 +5,33 @@ function [] = plot_particle_positions()
 particle_files = dir('mobile_*.dat');
 N_files = length(particle_files);
 
-particles = cell(1, N_files);
+p = cell(1, N_files);
 for mm = 1:N_files
     fname = sprintf('mobile_%d.dat', mm-1);
-    particles{mm} = readtable(fname);
+    p{mm} = readtable(fname);
 end
 
 figure(66)
 clf
 hold on
 
+time = p{1}.time;
+colormap(parula(length(time)))
+cmap = colormap(parula(length(time)));
 for mm = 1:N_files
-    %plot(particles{mm}.x, particles{mm}.y, 'k')
+    %plot(p{mm}.x, particles{mm}.y, 'k')
     %legend_label{mm} = sprintf('Particle %d',mm);
-    x = particles{mm}.x;
-    y = particles{mm}.y;
-    col = particles{mm}.time;
+    x = p{mm}.x;
+    y = p{mm}.y;
+    col = p{mm}.time;
     surface([x,x], [y,y], [col,col],...
         'facecol','no','edgecol','interp','linew',2);
+end
+
+%for ts = linspace(0,time(end), 10)
+for ts = 0:5:time(end)
+    ind = nearest_index(p{1}.time, ts);
+    plot([p{1}.x(ind) p{2}.x(ind)],[p{1}.y(ind) p{2}.y(ind)], 'Color', cmap(ind,:))
 end
 
 cbar = colorbar;
