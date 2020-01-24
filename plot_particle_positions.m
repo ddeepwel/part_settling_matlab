3af6686e1d5907a7b8f14cbead5b455f31b65837
David Deepwell <ddeepwel@uwaterloo.ca>
2020-01-24 15:47:21 -0500

    Minor changes to use check_read_dat in plot_particle_positions

--- plot_particle_positions.m
+++ plot_particle_positions.m
@@ -1,16 +1,18 @@
 function [] = plot_particle_positions()
-% plot particle positions
+% plot particle positions and colour by time
 
-
+% search for particle files
 particle_files = dir('mobile_*.dat');
 N_files = length(particle_files);
 
+% read particle files
 p = cell(1, N_files);
 for mm = 1:N_files
-    fname = sprintf('mobile_%d.dat', mm-1);
-    p{mm} = readtable(fname);
+    fname = sprintf('mobile_%d', mm-1);
+    p{mm} = check_read_dat(fname);
 end
 
+% make plot
 figure(66)
 clf
 hold on
@@ -20,15 +22,13 @@ Nt = length(time);
 colormap(parula(Nt))
 cmap = colormap(parula(Nt));
 for mm = 1:N_files
-    %plot(p{mm}.x, particles{mm}.y, 'k')
-    %legend_label{mm} = sprintf('Particle %d',mm);
     x = p{mm}.x;
     y = p{mm}.y;
     col = p{mm}.time;
     scatter(x,y,[],col,'fill','SizeData',8)
 end
 
-%for ts = linspace(0,time(end), 10)
+% add horizontal bands at intervals of 5 time units
 for ts = 0:5:time(end)
     ind = nearest_index(time, ts);
     cmap_ind = round(ts/time(end) * (Nt-1) + 1);
@@ -37,14 +37,12 @@ end
 
 cbar = colorbar;
 cbar.Label.String = '$t$';
-
 xlabel('$x/D_p$')
 ylabel('$y/D_p$')
 title('particle position')
-%legend_handle = legend(legend_label);
-%legend_handle.Location = 'Best';
 figure_defaults()
 
+% save figure
 check_make_dir('figures')
 cd('figures')
 print_figure('particle_positions','format','pdf','size',[6 4])
