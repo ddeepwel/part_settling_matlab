be092df275b64869d844d4ea98bc92f161faa394
David Deepwell <ddeepwel@uwaterloo.ca>
2020-01-24 16:57:47 -0500

    Add plot of the relative path of particle 2 wrt particle 1

---
+++ plot_particle_relpath.m
@@ -0,0 +1,41 @@
+function [] = plot_particle_positions()
+% plot relative particle path of particle 0
+% with respect to particle 1
+
+
+particle_files = dir('mobile_*.dat');
+N_files = length(particle_files);
+
+p = cell(1, N_files);
+for mm = 1:N_files
+    fname = sprintf('mobile_%d', mm-1);
+    p{mm} = check_read_dat(fname);
+end
+
+figure(71)
+%clf
+hold on
+
+time = p{1}.time;
+Nt = length(time);
+colormap(parula(Nt))
+cmap = colormap(parula(Nt));
+x = p{1}.x - p{2}.x;
+y = p{1}.y - p{2}.y;
+col = p{mm}.time;
+scatter(x,y,[],col,'fill','SizeData',8)
+%plot(x,y,'k')
+plot(0, 0, 'ko')
+
+cbar = colorbar;
+cbar.Label.String = '$t$';
+
+xlabel('$x/D_p$')
+ylabel('$y/D_p$')
+title('particle 2 path relative to particle 1')
+figure_defaults()
+
+check_make_dir('figures')
+cd('figures')
+print_figure('particle_relpath','format','pdf','size',[6 4])
+cd('..')
