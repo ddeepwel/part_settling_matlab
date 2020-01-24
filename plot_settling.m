26b9744712c51f3f1620f45f9aefb1f5aa87453a
David Deepwell <ddeepwel@uwaterloo.ca>
2020-01-24 16:49:32 -0500

    Update settling velocity plot

--- plot_settling.m
+++ plot_settling.m
@@ -5,43 +5,16 @@ if nargin == 0
     particle_ID = 0;
 end
 
-% check the version of the simulation
-if exist('mobile.dat','file') == 2
-    % old cases (1 particle) where the particle ID was not in the file name
-    old_version = true;
-    file_name = 'mobile';
-elseif exist('mobile_0.dat','file') == 2
-    % new cases where the particle ID IS in the file name
-    old_version = false;
-    file_name = sprintf('mobile_%d',particle_ID);
-else
-    error('mobile particles file not found.')
-end
-
 % Read data
-if exist([file_name,'.mat'], 'file') == 2
-    % load cleaned data, if it exists
-    diag_file = [file_name,'.mat'];
-    particle_data = load(diag_file);
-    % also load the txt file to check if mat file is up to date
-    diag_dat = readtable([file_name,'.dat']);
-else
-    % otherwise read raw data (this will likely be fine, unless a restart caused overlapping data)
-    diag_file = [file_name,'.dat'];
-    particle_data = readtable(diag_file);
-end
+file_name = sprintf('mobile_%d',particle_ID);
+particle_data = check_read_dat(file_name);
 
 % get data
-if old_version
-    time = particle_data.Var1;
-    y_p  = particle_data.Var4;
-else
-    time = particle_data.time;
-    y_p  = particle_data.y;
-end
+time = particle_data.time;
+y_p  = particle_data.y;
 
 % calculate velocity
-Dmat = FiniteDiff(time, 1, 2, false, false);
+Dmat = FiniteDiff(time, 1, 2, true, false); % some cases have the time step change
 vel = Dmat * y_p;
 
 
@@ -50,16 +23,18 @@ figure(65)
 %clf
 subplot(2,1,1)
 hold on
-plot(time, y_p)
+plot(time, y_p,'-')
 fprintf('Initial particle position: %g D_p\n',y_p(1))
 ylabel('$h_p/D_p$')
 title('particle height')
+grid on
 
 subplot(2,1,2)
 hold on
-plot(time, vel)
+plot(time, vel,'-')
 ylabel('$w_p/w_s$')
 xlabel('$t$')
 title('particle velocity')
+grid on
 
 figure_defaults()
--- plot_settling_multi.m
+++ plot_settling_multi.m
@@ -4,6 +4,7 @@ clf
 plot_settling(0);
 plot_settling(1);
 
+check_make_dir('figures')
 cd('figures')
 print_figure('settling','format','pdf','size',[6 4]);
 cd('..')
