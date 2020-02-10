1e479bc102107c7b0cfc442a9f05a43c07194fcf
David Deepwell <ddeepwel@uwaterloo.ca>
2020-02-10 16:26:05 -0500

    Add function for measuring the settling position/speed

---
+++ settling.m
@@ -0,0 +1,19 @@
+function [time, y_p, vel] = settling(particle_ID)
+% get the settling velocity and position of a particle
+
+if nargin == 0
+    particle_ID = 0;
+end
+
+% Read data
+file_name = sprintf('mobile_%d',particle_ID);
+particle_data = check_read_dat(file_name);
+
+% get data
+time = particle_data.time;
+y_p  = particle_data.y;
+
+% calculate velocity
+Dmat = FiniteDiff(time, 1, 2, true, false); % some cases have variable time step 
+vel = Dmat * y_p;
+
