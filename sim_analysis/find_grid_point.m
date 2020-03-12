7cef69b49226f6656298ea841811f74a62991932
David Deepwell <ddeepwel@uwaterloo.ca>
2020-03-12 16:03:50 -0400

    update find_grid_point to look for max and min of density

--- sim_analysis/find_grid_point.m
+++ sim_analysis/find_grid_point.m
@@ -1,3 +1,4 @@
+function [xmax_xind, max_yind, max_zind, min_xind, min_yind, min_zind] =  find_grid_point(ii)
 % find the grid location where the density is a maximum
 
 params = read_params();
@@ -5,12 +6,19 @@ Nx = params.NXM+1;
 Ny = params.NYM+1;
 Nz = params.NZM+1;
 
-rho10 = h5read('Data_10.h5','/Conc/0');
-vf10  = h5read('Data_10.h5','/vfc');
+filename = sprintf('Data_%d.h5',ii);
+rho = h5read(filename, '/Conc/0');
+vf  = h5read(filename, '/vfc');
+time= h5read(filename, '/time');
 
-[mx,ind] = max(rho10(:) .* (1-vf10(:)) );
+[max_val,max_ind] = max(rho(:) .* (1-vf(:)) );
+[min_val,min_ind] = min(rho(:) .* (1-vf(:)) );
 
-[xind, yind, zind] = ind2sub([Nx Ny Nz], ind);
+[max_xind, max_yind, max_zind] = ind2sub([Nx Ny Nz], max_ind);
+[min_xind, min_yind, min_zind] = ind2sub([Nx Ny Nz], min_ind);
 
-fprintf('max:  %.5g\n',mx)
-fprintf('grid: %.5g\n',rho10(xind,yind,zind) .* (1-vf10(xind,yind,zind)) )
+fprintf('t = %.3g\n', time)
+fprintf('max:  %.5g\n',max_val)
+fprintf('grid: %.5g\n',rho(max_xind,max_yind,max_zind) .* (1-vf(max_xind,max_yind,max_zind)) )
+fprintf('min:  %.5g\n',min_val)
+fprintf('grid: %.5g\n',rho(min_xind,min_yind,min_zind) .* (1-vf(min_xind,min_yind,min_zind)) )
