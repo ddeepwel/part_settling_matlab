c173032f66fca65d055a62b20496bad597c32aab
David Deepwell <ddeepwel@cedar1.cedar.computecanada.ca>
2020-04-01 10:26:40 -0700

    Add new function to find minimum separation distance b/w particles

---
+++ analysis/min_part_sep.m
@@ -0,0 +1,6 @@
+function min_sep = min_part_sep()
+% measure the minimum particle separation
+
+[time, sep, sep_vel] = particle_separation();
+[min_sep, min_sep_ind] = min(sep);
+min_sep_time = time(min_sep_ind);
