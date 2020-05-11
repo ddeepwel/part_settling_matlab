c8d31c345fd67b555bb19f27695df1eadfbe8e0e
David Deepwell <ddeepwel@uwaterloo.ca>
2020-05-11 18:16:48 -0400

    improve make_scalar_stats to run only if field exists

--- analysis/make_scalar_stats.m
+++ analysis/make_scalar_stats.m
@@ -6,20 +6,32 @@ fields = {'entrained_fluid','entrained_tracer'};
 fileformat = 'Data2d';
 
 for mm = 1:length(fields)
-    fieldname = ['/integral/',fields{mm}];
-
-    Noutputs = last_output(fileformat);
-    volume  = zeros(Noutputs,1);
-    time = zeros(Noutputs,1);
-
-    for ii = 1:Noutputs
-        filename_2d = sprintf([fileformat,'_%d.h5'],ii-1);
-        volume(ii)  = h5read(filename_2d, fieldname);
-        time(ii) = h5read(filename_2d, '/time');
-
-        completion(ii,Noutputs)
+    info = h5info('Data2d_0.h5','/integral');
+    for nn = 1:length(info.Datasets)
+        if strcmp(info.Datasets(nn).Name,fields{mm})
+            field_exist = true;
+            break
+        else
+            field_exist = false;
+        end
     end
 
-    save(fields{mm},'time','volume');
+    if field_exist
+        fieldname = ['/integral/',fields{mm}];
+
+        Noutputs = last_output(fileformat);
+        volume  = zeros(Noutputs,1);
+        time = zeros(Noutputs,1);
+
+        for ii = 1:Noutputs
+            filename_2d = sprintf([fileformat,'_%d.h5'],ii-1);
+            volume(ii)  = h5read(filename_2d, fieldname);
+            time(ii) = h5read(filename_2d, '/time');
+
+            completion(ii,Noutputs)
+        end
+
+        save(fields{mm},'time','volume');
+    end
 end
 % need to multiply volume by 6/pi to get ratio of entrained fluid to particle volume
